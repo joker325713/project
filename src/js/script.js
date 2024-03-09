948c1197d0dd8569cfee0b3505be180fba2005ae
Marsel <shaidullov-marsel@mail.ru>
2024-03-09 22:41:46 +0300

    carousel and tabs

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        /* adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>',
+
+
+    });
+  });
